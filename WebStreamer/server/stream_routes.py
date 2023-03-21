c6f7ea5d5e91ca01d28b04654dc93c0a38b38072
Kawin Raju <128094223+kktailors@users.noreply.github.com>
2023-03-21 18:58:00 +0530

    Update stream_routes.py

--- WebStreamer/server/stream_routes.py
+++ WebStreamer/server/stream_routes.py
@@ -16,7 +16,7 @@ routes = web.RouteTableDef()
 async def root_route_handler(request):
     bot_details = await StreamBot.get_me()
     return web.json_response({"status": "running",
-                              "maintained_by": "Avishkar_Patil",
+                              "maintained_by": "Kawin_raju",
                               "server_permission": "Open",
                               "Telegram_Bot": '@'+bot_details.username})
 
